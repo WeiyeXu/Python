851f30966384c8eb860cd35ee116d1462ad7f8eb
Weiy Xu <XuWeiyeSherry@gmail.com>
2019-04-25 14:08:53 -0600

    Create the first Application based on Python

---
+++ 小程序练习/题目 0013/picturePicker.py
@@ -0,0 +1,16 @@
+""" 
+小程序： 图片抓包
+@Author Weiye Xu
+Date: 4/25/219
+Finshed: 
+
+"""
+
+
+import os       # 导入系统控制包
+import webbrowser   # 导入浏览器控制
+
+# 打开网页
+webbrowser.open("https://www.pixiv.net/", new=2)
+
+os.system('"C:/Program Files/Internet Explorer/iexplore.exe" http://www.baidu.com')
